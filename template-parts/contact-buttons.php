b844fadecba41e2a45313fab7a03073c3626bd9e
rs6dtms8mn-wq <rs6dtms8mn@privaterelay.appleid.com>
2026-07-22 23:27:21 +0300

    Create contact-buttons.php

---
+++ template-parts/contact-buttons.php
@@ -0,0 +1,37 @@
+<div class="floating-buttons">
+
+<a 
+
+href="tel:+70000000000"
+
+class="call-button">
+
+📞 Позвонить
+
+</a>
+
+<a 
+
+href="https://t.me/ВАШ_USERNAME"
+
+class="telegram-button"
+
+target="_blank">
+
+✈️ Telegram
+
+</a>
+
+<a 
+
+href="https://max.ru/ВАШ_АККАУНТ"
+
+class="max-button"
+
+target="_blank">
+
+💬 MAX
+
+</a>
+
+</div>
