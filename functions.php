d0f92ac5e6fdc6230f2a01cd880e21ba39cd2e52
rs6dtms8mn-wq <rs6dtms8mn@privaterelay.appleid.com>
2026-07-22 21:31:15 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+function motortech_setup(){
+
+    add_theme_support('title-tag');
+
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus(array(
+
+        'main_menu'=>'Главное меню'
+
+    ));
+
+}
+
+add_action(
+
+    'after_setup_theme',
+
+    'motortech_setup'
+
+);
+
+function motortech_scripts(){
+
+    wp_enqueue_style(
+
+        'motortech-style',
+
+        get_stylesheet_uri()
+
+    );
+
+}
+
+add_action(
+
+    'wp_enqueue_scripts',
+
+    'motortech_scripts'
+
+);
+
+?>
