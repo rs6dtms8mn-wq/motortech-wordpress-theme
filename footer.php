3696b77337891927226b491d4d562476873c40d1
rs6dtms8mn-wq <rs6dtms8mn@privaterelay.appleid.com>
2026-07-22 21:33:48 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+
+<div class="container">
+
+<h3>МОТÖРТЕХ</h3>
+
+<p>
+
+Ремонт радиаторов • Шиномонтаж •
+
+Ремонт автомобилей • Спецтехника
+
+</p>
+
+<p>
+
+© <?php echo date('Y'); ?> МОТÖРТЕХ
+
+</p>
+
+</div>
+
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
