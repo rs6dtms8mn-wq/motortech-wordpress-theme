b44b994a42bb881bfb4120eda9f5eefe4171a81d
rs6dtms8mn-wq <rs6dtms8mn@privaterelay.appleid.com>
2026-07-22 23:00:16 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,36 @@
 <?php
 
+/*
+
+MOTÖРТЕХ Theme Functions
+
+*/
+
+// Настройки темы
+
 function motortech_setup(){
 
-    add_theme_support('title-tag');
+    add_theme_support(
 
-    add_theme_support('post-thumbnails');
+        'title-tag'
 
-    register_nav_menus(array(
+    );
 
-        'main_menu'=>'Главное меню'
+    add_theme_support(
 
-    ));
+        'post-thumbnails'
+
+    );
+
+    register_nav_menus(
+
+        array(
+
+            'main_menu' => 'Главное меню'
+
+        )
+
+    );
 
 }
 
@@ -22,31 +42,33 @@ add_action(
 
 );
 
+// Подключение CSS и JS
+
 function motortech_scripts(){
 
-wp_enqueue_style(
+    wp_enqueue_style(
 
-'motortech-style',
+        'motortech-style',
 
-get_stylesheet_uri()
+        get_stylesheet_uri()
 
-);
+    );
 
-wp_enqueue_script(
+    wp_enqueue_script(
 
-'motortech-calculator',
+        'motortech-calculator',
 
-get_template_directory_uri()
+        get_template_directory_uri()
 
-.'/assets/js/calculator.js',
+        .'/assets/js/calculator.js',
 
-array(),
+        array(),
 
-null,
+        null,
 
-true
+        true
 
-);
+    );
 
 }
 
@@ -57,11 +79,21 @@ add_action(
     'motortech_scripts'
 
 );
+
+// Настройки темы
+
 require_once get_template_directory()
 
 .'/inc/theme-options.php';
 
+// Заявки клиентов
+
 require_once get_template_directory()
 
 .'/inc/custom-post-types.php';
-?>
+
+// Обработка формы
+
+require_once get_template_directory()
+
+.'/inc/form-handler.php';
